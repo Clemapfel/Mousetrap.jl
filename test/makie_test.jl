b58a224ae37b76b523c2ccbe8bdebdc2aaa3a18d
Clem Cords <mail@clemens-cords.com>
2023-10-13 16:03:58 +0200

    working

--- test/makie_test.jl
+++ test/makie_test.jl
@@ -5,10 +5,26 @@ using GLMakie: empty_postprocessor, fxaa_postprocessor, OIT_postprocessor, to_sc
 using GLMakie.GLAbstraction
 using GLMakie.Makie
 
+# maps hash(GLMakieArea) too GLMakie.Screen
 const screens = Dict{UInt64, GLMakie.Screen}()
 
-mutable struct GLMakieArea <: Widget
+"""
+## GLMakieArea <: Widget
+`GLArea` wrapper that automatically connects all necessary callbacks in order for it to be used the GLMakie render target 
 
+## Constructors
+`GLMakieArea()`
+
+## Signals
+(no unique signals)
+
+## Fields
+(no public fields)
+
+## Example
+TODO
+"""
+mutable struct GLMakieArea <: Widget
     glarea::GLArea
     framebuffer_id::Ref{Int}
     framebuffer_size::Vector2i
@@ -23,10 +39,7 @@ mutable struct GLMakieArea <: Widget
 end
 Mousetrap.get_top_level_widget(x::GLMakieArea) = x.glarea
 
-function GLMakie.resize_native!(native::GLMakieArea, resolution...)
-    # noop, ignore makie size request to not interfere with widget layout manager
-end
-
+# render callback: if screen is open, render frame to `GLMakieArea`s OpenGL context
 function on_makie_area_render(self, context)
     key = Base.hash(self)
     if haskey(screens, key)
@@ -40,6 +53,7 @@ function on_makie_area_render(self, context)
     return true
 end
 
+# resize callback: update framebuffer size, necessary for `GLMakie.framebuffer_size`
 function on_makie_area_resize(self, w, h)
     key = Base.hash(self)
     if haskey(screens, key)
@@ -52,29 +66,46 @@ function on_makie_area_resize(self, w, h)
     return nothing
 end
 
+# resolution of `GLMakieArea` OpenGL framebuffer
+GLMakie.framebuffer_size(self::GLMakieArea) = (self.framebuffer_size.x, self.framebuffer_size.y)
+
+# forward retina scale factor from GTK4 back-end
+GLMakie.retina_scaling_factor(w::GLMakieArea) = Mousetrap.get_scale_factor(w)
+
+# resolution of `GLMakieArea` widget itself`
 function GLMakie.window_size(w::GLMakieArea)
     size = get_natural_size(w)
     size.x = size.x * GLMakie.retina_scaling_factor(w)
     size.y = size.y * GLMakie.retina_scaling_factor(w)
     return (size.x, size.y)
 end
-GLMakie.framebuffer_size(self::GLMakieArea) = (self.framebuffer_size.x, self.framebuffer_size.y)
 
-function GLMakie.was_destroyed(window::GLMakieArea)
-    return !get_is_realized(window)
+# calculate screen size and dpi
+function Makie.window_area(scene::Scene, screen::GLMakie.Screen{GLMakieArea})
+    glarea = screen.glscreen
+    scene.events.window_area[] = Recti(0, 0, glarea.framebuffer_size.x, glarea.framebuffer_size.y)
+    scene.events.window_dpi[] = Mousetrap.calculate_monitor_dpi(glarea)
 end
 
-function Base.isopen(w::GLMakieArea)
-    return !GLMakie.was_destroyed(w)
+# resize request by makie will be ignored
+function GLMakie.resize_native!(native::GLMakieArea, resolution...)
+    # noop
 end
 
-function GLMakie.set_screen_visibility!(screen::GLMakieArea, bool)
-    if bool 
-        show!(screen.glarea)
-    else
-        hide!(screen.glarea)
-    end
+# bind `GLMakieArea` OpenGL context
+ShaderAbstractions.native_switch_context!(a::GLMakieArea) = make_current(a.glarea)
+
+# check if `GLMakieArea` OpenGL context is still valid, it is while `GLMakieArea` widget stays realized
+ShaderAbstractions.native_context_alive(x::GLMakieArea) = get_is_realized(x)
+
+# destruction callback ignored, lifetime is managed by mousetrap instead
+function GLMakie.destroy!(w::GLMakieArea)
+    # noop
 end
+
+GLMakie.was_destroyed(window::GLMakieArea) = !get_is_realized(window)
+Base.isopen(w::GLMakieArea) = !GLMakie.was_destroyed(w)
+GLMakie.set_screen_visibility!(screen::GLMakieArea, bool) = bool ? show(screen.glarea) : hide!(screen.glarea)
 
 function GLMakie.apply_config!(screen::GLMakie.Screen{GLMakieArea}, config::GLMakie.ScreenConfig; start_renderloop=true) 
     @warn "In MousetrapMakie: GLMakie.apply_config!: This feature is not yet implemented, ignoring config"
@@ -83,19 +114,29 @@ function GLMakie.apply_config!(screen::GLMakie.Screen{GLMakieArea}, config::GLMa
 end
 
 function Makie.colorbuffer(screen::GLMakie.Screen{GLMakieArea}, format::Makie.ImageStorageFormat = Makie.JuliaNative)
-
     @warn "In MousetrapMakie: GLMakie.colorbuffer: This feature is not yet implemented, returning framecache"
     # cf https://github.com/JuliaGtk/Gtk4Makie.jl/blob/main/src/screen.jl#L147
     return screen.framecache
 end
 
-ShaderAbstractions.native_switch_context!(a::GLMakieArea) = make_current(a.glarea)
-ShaderAbstractions.native_context_alive(x::GLMakieArea) = get_is_realized(x)
+# ignore makie event model, use the mousetrap event controllers instead
+Makie.disconnect!(window::GLMakieArea, f) = nothing
+GLMakie.pollevents(::GLMakie.Screen{GLMakieArea}) = nothing
+Makie.mouse_buttons(scene::Scene, glarea::GLMakieArea) = nothing
+Makie.keyboard_buttons(scene::Scene, glarea::GLMakieArea) = nothing
+Makie.dropped_files(scene::Scene, window::GLMakieArea) = nothing
+Makie.unicode_input(scene::Scene, window::GLMakieArea) = nothing
+Makie.mouse_position(scene::Scene, screen::GLMakie.Screen{GLMakieArea}) = nothing
+Makie.scroll(scene::Scene, window::GLMakieArea) = nothing
+Makie.hasfocus(scene::Scene, window::GLMakieArea) = nothing
+Makie.entered_window(scene::Scene, window::GLMakieArea) = nothing
 
-function GLMakie.destroy!(w::GLMakieArea)
-    # noop, lifetime is managed by mousetrap instead
-end
-    
+"""
+```
+create_gl_makie_screen(::GLMakieArea; screen_config...) -> GLMakie.Screen{GLMakieArea}
+```
+For a `GLMakieArea`, create a GLMakie screen that can be used to display makie graphics
+"""
 function create_glmakie_screen(area::GLMakieArea; screen_config...)
 
     if !get_is_realized(area) 
@@ -110,7 +151,7 @@ function create_glmakie_screen(area::GLMakieArea; screen_config...)
     set_is_visible!(area, config.visible)
     set_expand!(area, true)
 
-    # copy of https://github.com/JuliaGtk/Gtk4Makie.jl/blob/main/src/screen.jl#L342
+    # quote from https://github.com/JuliaGtk/Gtk4Makie.jl/blob/main/src/screen.jl#L342
     shader_cache = GLAbstraction.ShaderCache(area)
     ShaderAbstractions.switch_context!(area)
     fb = GLMakie.GLFramebuffer((1, 1)) # resized on GLMakieArea realization later
@@ -134,11 +175,10 @@ function create_glmakie_screen(area::GLMakieArea; screen_config...)
         Dict{UInt32, Makie.AbstractPlot}(),
         false,
     )
-    # end copy
+    # end quote
 
     hash = Base.hash(area.glarea)
     screens[hash] = screen
-    println("register: $hash")
     
     set_tick_callback!(area.glarea) do clock::FrameClock
         if GLMakie.requires_update(screen)
@@ -155,67 +195,18 @@ function create_glmakie_screen(area::GLMakieArea; screen_config...)
     return screen
 end
 
-Makie.disconnect!(window::GLMakieArea, f) = nothing
-Makie.window_open(scene::Scene, window::GLMakieArea) = nothing
-
-function Makie.window_area(scene::Scene, screen::GLMakie.Screen{GLMakieArea})
-    area = scene.events.window_area
-    dpi = scene.events.window_dpi
-    glarea = screen.glscreen
-
-    function on_resize(self, w, h)
-        dpi[] = Mousetrap.calculate_monitor_dpi(glarea)
-        area[] = Recti(0, 0, w, h)
-        return nothing
-    end
-
-    connect_signal_resize!(on_resize, glarea.glarea)
-    queue_render(glarea.glarea)
-end
-
-function GLMakie.retina_scaling_factor(window::GLMakieArea)
-    return Mousetrap.get_scale_factor(window.glarea)
-end
-
-# ignore makie event model, use the mousetrap event controllers instead
-GLMakie.pollevents(::GLMakie.Screen{GLMakieArea}) = nothing
-Makie.mouse_buttons(scene::Scene, glarea::GLMakieArea) = nothing
-Makie.keyboard_buttons(scene::Scene, glarea::GLMakieArea) = nothing
-Makie.dropped_files(scene::Scene, window::GLMakieArea) = nothing
-Makie.unicode_input(scene::Scene, window::GLMakieArea) = nothing
-Makie.mouse_position(scene::Scene, screen::GLMakie.Screen{GLMakieArea}) = nothing
-Makie.scroll(scene::Scene, window::GLMakieArea) = nothing
-Makie.hasfocus(scene::Scene, window::GLMakieArea) = nothing
-Makie.entered_window(scene::Scene, window::GLMakieArea) = nothing
-
-mutable struct MakieCanvas <: Widget
-    area::GLMakieArea
-    screen::Ref{Union{GLMakie.Screen{GLMakieArea}, Nothing}}
-
-    function MakieCanvas()
-        area = GLMakieArea()
-        out = new(area, nothing)
-        connect_signal_realize!(on_makie_canvas_realize, area.glarea, out)
-        return out
-    end
-end
-Mousetrap.get_top_level_widget(x::MakieCanvas) = x.area
-
-function on_makie_canvas_realize(self::GLArea, out::MakieCanvas)
-    # screen initialization has to be delayed until underlying widget is realized
-    # because the OpenGL context of `GLArea` can only be created once the widget is visible on screen
-    make_current(self)
-    out.screen[] = create_glmakie_screen(out.area)
-    return nothing
-end
 
 main() do app::Application
     window = Window(app)
-    canvas = MakieCanvas()
+    canvas = GLMakieArea()
     set_size_request!(canvas, Vector2f(200, 200))
     set_child!(window, canvas)
-    connect_signal_show!(window) do self::Window
-        display(canvas.screen[], scatter(1:4))
+
+    screen = Ref{Union{Nothing, GLMakie.Screen{GLMakieArea}}}(nothing)
+
+    connect_signal_realize!(canvas) do self
+        screen[] = create_glmakie_screen(canvas)
+        display(screen[], scatter(1:4))
         return nothing
     end
     present!(window)
