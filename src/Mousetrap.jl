91d5ddeef8387c5eb0804624b0b4f9ce43f35e7d
Clem Cords <mail@clemens-cords.com>
2023-11-13 17:44:46 +0100

    polish gsettings env var setting

--- src/Mousetrap.jl
+++ src/Mousetrap.jl
@@ -22,14 +22,18 @@ module Mousetrap
 
     module detail
         using CxxWrap
-        import GTK4_jll 
-        function __init__() 
-            
-            # use GTK4_jlls settings schema if none are available on the machine
-            if !Sys.islinux() && (!haskey(ENV, "GSETTINGS_SCHEMA_DIR") || isempty(ENV["GSETTINGS_SCHEMA_DIR"]))
+        import GTK4_jll, Glib_jll
+        function try_update_gsettings_schema_dir()
+            # request to use GTK4_jll-supplied settings schema if none are available on the machine
+            schema_maybe = @ccall Glib_jll.libgio.g_settings_schema_source_get_default()::Ptr{Cvoid}
+            if schema_maybe == C_NULL && (!haskey(ENV, "GSETTINGS_SCHEMA_DIR") || isempty(ENV["GSETTINGS_SCHEMA_DIR"]))
+                @info "setting GSETTINGS_SECHMAD"
                 ENV["GSETTINGS_SCHEMA_DIR"] = normpath(joinpath(GTK4_jll.libgtk4, "../../share/glib-2.0/schemas"))
             end
-            println("called")
+        end
+
+        function __init__() 
+            try_update_gsettings_schema_dir()   # executed on `using Mousetrap`, env needs to be set each time before `adw_init` is called
             @initcxx()
         end
 
@@ -38,6 +42,7 @@ module Mousetrap
             return mousetrap_jll.mousetrap_julia_binding
         end
       
+        try_update_gsettings_schema_dir()       # executed on `precompile Mousetrap`, but not on using, silences warning during installation
         @wrapmodule(get_mousetrap_julia_binding)
     end
 
